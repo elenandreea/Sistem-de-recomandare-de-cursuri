548d3b48c0dacb5326767bc64fce0df747c6c3d2
andreea <andreeagorgoi@gmail.com>
2020-12-09 00:30:39 +0200

    incarcare tabela useri

--- DatasetManipulation/UsersExtraction.py
+++ DatasetManipulation/UsersExtraction.py
@@ -18,8 +18,8 @@ def get_id_and_url():
 
     result = {}
     for d in data:
-        value = d[1][1:-1]
-        result[d[0]] = value.split("https://coursera.org/learn/")[1]
+        key = d[1][1:-1].split("https://coursera.org/learn/")[1]
+        result[key] = d[0]
 
     return result
 
@@ -28,18 +28,40 @@ def get_id_and_url():
 def create_reviews_table():
     db = mysql.connector.connect(host='localhost', user='root', passwd='admin', database='recommendation')
     mycursor = db.cursor()
-    mycursor.execute(
-        "CREATE TABLE reviews(id_curs INT NOT NULL, user MEDIUMTEXT, rating DOUBLE)")
+    mycursor.execute("CREATE TABLE reviews(id_curs INT NOT NULL, user TEXT, rating DOUBLE)")
     db.close()
 
 
-#NOT DONE
 def get_users_review():
+
+    rows_db = get_id_and_url()
+    keys_db = rows_db.keys()
+
     with open('../Datasets/Coursera_reviews.csv', newline='', encoding="utf-8") as csv_file:
         csv_reader = csv.DictReader(csv_file)
 
         result = {}
+        for row_csv in csv_reader:
+            if row_csv['course_id'] in keys_db:
+                reviewer = row_csv['reviewers'][3:]
+                course_id = rows_db[row_csv['course_id']]
+                if (reviewer, row_csv['course_id']) not in result.keys():
+                    result[(reviewer, course_id)] = float(row_csv['rating'])
         return result
+
+
+def write_users_review_to_csv():
+    result = get_users_review()
+    print(len(result))
+
+    with open('../Datasets/new_users_review.csv', 'w', encoding='utf-8') as new_csv_file:
+        fieldnames = ['course_id', 'reviewer', 'rating']
+
+        csv_writer = csv.writer(new_csv_file)
+        csv_writer.writerow(fieldnames)
+
+        for reviewer, course_id in result:
+            csv_writer.writerow([reviewer, course_id, result[reviewer, course_id]])
 
 
 #NOT DONE
@@ -49,18 +71,19 @@ def insert_into_users_table():
     cursor = mydb.cursor()
     result = get_users_review()
 
-    for row in result:
+    for reviewer, course_id in result:
         try:
             cursor.execute('INSERT INTO reviews(id_curs, user, rating) VALUES("%s", "%s", "%s")',
-                           [row['id_curs'], row['user'], float(row['rating'])])
+                           [course_id, reviewer, result[reviewer, course_id]])
         except Exception as e:
             print(e)
-            print(row)
+            print(reviewer, course_id)
             break
     mydb.commit()
     cursor.close()
 
 
 if __name__ == '__main__':
-    create_reviews_table()
-    # print(get_id_and_url())
+    # create_reviews_table()
+    # write_users_review_to_csv()
+    insert_into_users_table()
